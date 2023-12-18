e705f5f9f560b0900c5c909955667408b27f7519
ZakaDes <zakarialahmiyed@gmail.com>
2023-12-18 05:30:22 -0800

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Print all possible combinations of two digits
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	int num1;
+	int num2;
+
+	for (num1 = '0'; num1 <= '8'; num1++)
+	{
+		for (num2 = num1 + 1; num2 <= '9'; num2++)
+		{
+			putchar (num1);
+			putchar (num2);
+			putchar (',');
+			putchar (' ');
+		}
+	}
+	putchar ('\n');
+	return (0);
+
+
+}
