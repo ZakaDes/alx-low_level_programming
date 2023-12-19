b773ad7db2ad19d3eb0196ffdce409b8753d93d6
ZakaDes <zakarialahmiyed@gmail.com>
2023-12-19 07:56:24 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * Print the alphabet in lowercase
+ * Return: 0 (success)
+ */
+
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 97; i <= 122; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
