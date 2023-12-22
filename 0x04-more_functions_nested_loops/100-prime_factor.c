74eec213c41c7efcbe45fa3fe3fa8ae72cc552f3
ZakaDes <zakarialahmiyed@gmail.com>
2023-12-22 11:57:29 -0800

    100-prime_factor.c

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Find the largest prime factor of 612852475143
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	long i, j;
+
+	i = 612852475143;
+
+	for (j = 2; j > i; j++)
+	{
+		while (i % j == 0)
+		{
+			i = i / j;
+		}
+	}
+	printf("%lu", j);
+	printf("\n");
+
+	return (0);
+}
