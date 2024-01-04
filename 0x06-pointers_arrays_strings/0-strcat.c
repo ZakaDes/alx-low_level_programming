445940099398dc54384fe13c8fc52d9ef331bf88
ZakaDes <zakarialahmiyed@gmail.com>
2024-01-04 06:31:24 -0800

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * *_strcat - concatenate two string.
+ * @dest: char pointer
+ * @src: second char pointer
+ * Return: res pointer
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int lenght = 0;
+	int count;
+	int i = 0;
+
+	while (dest[lenght] != '\0')
+	{
+		lenght++;
+	}
+
+
+	for (count = lenght; src[i] != '\0'; count++)
+	{
+		*(dest + count) = src[i];
+		i++;
+	}
+
+	return (dest);
+}
