83de647fa5e7950694ef534402a69451b1cfd2c1
ZakaDes <zakarialahmiyed@gmail.com>
2024-01-26 13:46:14 -0800

    2-print_strings.c

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -25,7 +25,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 		else
 		{
-			printf("nil");
+			printf("(nil)");
 		}
 
 		if (separator && i < n - 1)
@@ -33,6 +33,6 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			printf("%s", separator);
 		}
 	}
-	printf("\n");
 	va_end(arg);
+	printf("\n");
 }
