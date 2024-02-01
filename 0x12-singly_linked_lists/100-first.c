3c42d9ca4f9146aad10ef0876707e0a90d575eec
ZakaDes <zakarialahmiyed@gmail.com>
2024-02-01 12:49:36 -0800

    100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,16 +1,17 @@
 #include "lists.h"
 
+void main_print(void) __attribute__((constructor));
+
 /**
  * main_print - a function that prints a string before the main function is
  * exercuted
  *
  */
 
+
 void main_print (void)
 {
-	char *str;
 
-	str = "You're beat! and yet, you must allow,\nI bore my house upon my back!\n";
-
-	printf("%s\n", str);
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
