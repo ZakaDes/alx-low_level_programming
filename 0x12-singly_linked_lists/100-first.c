be944f8495548d6adf64e14276b282b08049385b
ZakaDes <zakarialahmiyed@gmail.com>
2024-02-01 12:35:08 -0800

    100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+
+/**
+ * main_print - a function that prints a string before the main function is
+ * exercuted
+ *
+ */
+
+void main_print (void)
+{
+	char *str;
+
+	str = "You're beat! and yet, you must allow,\nI bore my house upon my back!\n";
+
+	printf("%s\n", str);
+}
