495da7f08c63e1f6887329c62005ec1829b233a7
ZakaDes <zakarialahmiyed@gmail.com>
2024-02-06 13:12:55 -0800

    9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -38,6 +38,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			newNode->next = temp->next;
 			temp->next = newNode;
 			return (newNode);
+			i++;
 		}
 		else
 		{
